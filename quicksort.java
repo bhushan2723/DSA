2c5939f066473c01fa056aadbbfa71374761e485
bhushan2723 <bhushu2723@gmail.com>
2024-12-29 20:12:54 +0530

    Create quicksort.java

---
+++ quicksort.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        int temp; 
+        for (int j = low; j < high; j++) 
+        { 
+            if (arr[j] < pivot) {
+                i++;
+                // Swap
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quicksort(int arr[], int low, int high) {
+        if (low < high) {
+            int pidx = partition(arr, low, high);
+            quicksort(arr, low, pidx - 1);
+            quicksort(arr, pidx + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {90,30,5,2,0,7};
+        int n = arr.length;
+        quicksort(arr, 0, n - 1);
+
+        // Print
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " "); 
+        }
+        System.out.println(); 
+    }
+}
+
+/* output
+0 2 5 7 30 90 
+*/
+
+
+
